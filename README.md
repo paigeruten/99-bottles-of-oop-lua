825d4ba330f65aa0dd326e452ff535f671ff457e
Paige Ruten <paige.ruten@gmail.com>
2023-11-17 01:43:32 -0600

    Fix tabs/spaces, add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# 99 Bottles of OOP in Lua
+
+This is a Lua version of 99 Bottles of OOP (https://sandimetz.com/99bottles).
+
+## Usage
+
+Print out the song:
+
+    $ lua bottles.lua
+
+Run the tests:
+
+    $ TEST=1 lua bottles.lua
--- stdlib.lua
+++ stdlib.lua
@@ -1,9 +1,9 @@
 function extends(superclass, subclass)
-	subclass = subclass or {}
-	setmetatable(subclass, { __index = superclass })
-	return subclass
+  subclass = subclass or {}
+  setmetatable(subclass, { __index = superclass })
+  return subclass
 end
 
 function string.capitalize(s)
-	return string.upper(string.sub(s, 1, 1)) .. string.sub(s, 2)
+  return string.upper(string.sub(s, 1, 1)) .. string.sub(s, 2)
 end
